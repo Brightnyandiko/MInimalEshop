9b890c8e256f19b91799624bcec25ab0b1e0c370
Brightnyandiko <carlsonbright98@gmail.com>
2025-02-11 16:27:35 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_eshop/pages/intropage.dart';
+import 'package:minimal_eshop/themes/light_mode.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: IntroPage(),
+      theme: lightMode,
+    );
+  }
+}
+
