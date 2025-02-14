35167ba11dee49f5b921d80b368b99ab1b797600
Brightnyandiko <carlsonbright98@gmail.com>
2025-02-14 17:19:12 +0300

    made changes in the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_eshop/models/shop.dart';
 import 'package:minimal_eshop/pages/cart_page.dart';
 import 'package:minimal_eshop/pages/intropage.dart';
 import 'package:minimal_eshop/pages/profile_page.dart';
 import 'package:minimal_eshop/pages/shop_page.dart';
 import 'package:minimal_eshop/themes/light_mode.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,16 +16,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: IntroPage(),
-      theme: lightMode,
-      routes: {
-        '/intropage': (context) => const IntroPage(),
-        '/shop_page': (context) => const ShopPage(),
-        '/cart_page': (context) => const CartPage(),
-        '/profile_page': (context) => const ProfilePage()
-      },
+    return ChangeNotifierProvider(
+        create: (context) => Shop(),
+        builder: (context, child) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: IntroPage(),
+          theme: lightMode,
+          routes: {
+            '/intropage': (context) => const IntroPage(),
+            '/shop_page': (context) => const ShopPage(),
+            '/cart_page': (context) => const CartPage(),
+            '/profile_page': (context) => const ProfilePage()
+          },
+        ),
     );
   }
 }
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,13 +3,13 @@ class Product {
   String? name;
   double? price;
   String? description;
-  String? imagePath;
+  // String? imagePath;
 
   Product({
     required this.name,
     required this.price,
     required this.description,
-    required this.imagePath,
+    // required this.imagePath,
 });
 
 }
