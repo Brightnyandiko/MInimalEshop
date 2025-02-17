38a444a1dd6e785e1e1ba325c7d94c2557d4d208
Brightnyandiko <carlsonbright98@gmail.com>
2025-02-17 23:59:41 +0300

    checking whether there are any changes made to the app

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:minimal_eshop/components/my_drawer.dart';
+import 'package:minimal_eshop/models/shop.dart';
+import 'package:provider/provider.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final product = context.watch<Shop>().shop;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
